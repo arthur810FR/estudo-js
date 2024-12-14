ce50ecce1c3b215e078db2602a04bcc92e451802
Arthur Azevedo <arthurfranciscoazevedo@gmail.com>
2024-12-14 13:48:24 -0300

    Finalizacao da logica do Exer05

--- Exer05.js
+++ Exer05.js
@@ -9,10 +9,16 @@
     - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
 */
 
-const precoProduto = 100;
+const precoProduto = 85;
 
 const formaDePagamento = 1;
 
-if (precoProduto === 1) {
+if (formaDePagamento === 1) {
     console.log(precoProduto - (precoProduto * 0.1))
+} else if (formaDePagamento === 2){
+    console.log(precoProduto - (precoProduto * 0.15))
+} else if (formaDePagamento === 3){
+    console.log(precoProduto)
+} else {
+    console.log(precoProduto + (precoProduto * 0.1))
 }
