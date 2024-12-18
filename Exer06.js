91dc3dffdda0e8d3aac6ff6892a06d3d99b6b8a8
arthur810FR <arthurfranciscoazevedo@gmail.com>
2024-12-18 00:33:57 -0300

    subindo atvs excluidas

---
+++ Exer06.js
@@ -0,0 +1,11 @@
+function verificarIdade(idade){
+    if(idade >= 18){
+        console.log('Maior de idade');
+    } else {
+        console.log('Menor de idade');
+    }
+}
+
+
+
+verificarIdade(14);
