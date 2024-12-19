7e8287bfab6780bf8d4ea78a1e4714b315409571
arthur810FR <arthurfranciscoazevedo@gmail.com>
2024-12-19 19:52:01 -0300

    Criando a classe carro do Exer07

---
+++ Exer07.js
@@ -0,0 +1,11 @@
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca,cor,gastoMedioPorKm){
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+}
