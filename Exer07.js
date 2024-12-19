dcb19b2119acb9340f604a6a9c588488e8ead8a3
arthur810FR <arthurfranciscoazevedo@gmail.com>
2024-12-19 20:09:18 -0300

    Testando a classe Carro e colocando o desafio

--- Exer07.js
+++ Exer07.js
@@ -1,3 +1,10 @@
+/*
+Crie uma classe para representar carros. 
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
+gasto em reais para realizar este percurso.
+*/
+
 class Carro {
     marca;
     cor;
@@ -9,3 +16,7 @@ class Carro {
         this.gastoMedioPorKm = gastoMedioPorKm;
     }
 }
+
+const uno = new Carro('Fiat', 'Preto', 1/12);
+
+console.log(uno)
