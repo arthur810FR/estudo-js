22ace26d0163e40d89a7ada1db25ea63319d4728
arthur810FR <arthurfranciscoazevedo@gmail.com>
2024-12-20 20:32:37 -0300

    Finalizando o Exer08

--- Exer08.js
+++ Exer08.js
@@ -21,8 +21,23 @@ class Pessoa{
         return this.peso / Math.pow(this.altura, 2);
     }
     
+    classificarImc() {
+        const imc = this.calcularImc();
+        if (imc < 18.5) {
+            return 'Abaixo do peso';
+        } else if (imc >= 18.5 && imc < 25) {
+            return 'Peso normal';
+        } else if (imc >= 25 && imc < 30) {
+            return 'Acima do peso';
+        } else if (imc >= 30 && imc < 40) {
+            return 'Obeso';
+        } else {
+            return 'Obsedidade Grave';
+        }
+    }
+    
 }
 
 const Arthur = new Pessoa('Arthur', 56, 1,75);
-console.log(Arthur.calcularImc());
+console.log(Arthur.classificarImc());
 
